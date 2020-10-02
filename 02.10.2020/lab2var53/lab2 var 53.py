4fca177ef5b2ef77fad38f6d2657a24d2672a361
danamir <danamir522@gmail.com>
2020-10-02 19:36:41 +0300

    lab2v53

---
+++ 02.10.2020/lab2var53/lab2 var 53.py
@@ -0,0 +1,22 @@
+import math
+
+k = 1
+m = 2
+c = 3
+
+x1 = m ** 2 - k ** 2 - 4 * m * c
+
+if x1 >= 0:
+    x2 = math.sqrt(x1)
+else:
+    x2 = math.fabs(x1)
+
+if x1 > x2:
+    xmax = x1
+else:
+    xmax = x2
+
+print("X1 = " + str(x1))
+print("X2 = " + str(x2))
+print("Xmax = " + str(xmax))
+
